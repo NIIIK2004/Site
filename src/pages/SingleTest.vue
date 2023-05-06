6d3404e7c06e7d23e97afd69ef98cdfd64a80c2d
Nikita <nikitaxxx840@gmail.com>
2023-05-06 11:19:01 +0300

    Изменено

--- src/pages/SingleTest.vue
+++ src/pages/SingleTest.vue
@@ -3,8 +3,12 @@
 </script>
 
 <template>
-    <section class="section-title">
-        <h1 class="page-title">Определите свой уровень английского, пройдя простой тест </h1>
+    <section class="singletest">
+        <div class="container">
+            <button class="singles__button btn">Пропустить вступительный тест</button>
+            <h1 class="singletest__title page-title">Определите свой уровень английского, пройдя простой тест </h1>
+            <p class="singletest__description">Проверьте свои знания за 10 минут в простом онлайн-тесте. Но сначала нам нужно знать, для каких целей вы учите язык</p>
+        </div>
     </section>
 </template>
 
