24bd7501198be3fffbb0c88b52f90200e1f17846
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2023-08-19 12:52:34 +0300

    Update nodemailer.js

--- src/MailSander/nodemailer.js
+++ src/MailSander/nodemailer.js
@@ -5,13 +5,13 @@ export async function sendEmail(name, surname, email, phone, teacher) {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'yourihelo1971@gmail.com',
+            user: 'test@gmail.com',
             pass: 'asd58rcd'
         }
     });
 
     await transporter.sendMail({
-        from: 'yourihelo1971@gmail.com',
+        from: 'test@gmail.com',
         to: email,
         subject: 'Подтверждение записи на встречу',
         text: `Здравствуйте ${name} ${surname},\nвам была назначена встреча на 10.06 в 11:30 по адресу Шалимово д5 с преподавателем ${teacher}.`
